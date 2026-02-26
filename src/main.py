f7da106db1ca34660d54296d3ff0a7f2200f02cc
JMANY42 <2187.j.man@gmail.com>
2026-02-26 11:37:18 -0600

    feat(outlook): get emails from inbox

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 from elearning import assignment_due_soon, assignment_submitted
-
+from outlook import get_emails
 def preliminary_sort(message):
     subject = message["subject"]
-    sender_email = message["sender"]["emailAddress"]["address"]
+    from_email = message["from"]["emailAddress"]["address"]
 
     # Sort by email type for future processes
-    if sender_email == 'eLearning-Notification@utdallas.edu' and 'due soon' in subject:
+    if from_email == 'eLearning-Notification@utdallas.edu' and 'due soon' in subject:
         assignment_due_soon(message)
     
-    if sender_email == 'eLearning-Notification@utdallas.edu' and 'Submission received' in subject:
+    if from_email == 'eLearning-Notification@utdallas.edu' and 'Submission received' in subject:
         assignment_submitted(message)
 
-    elif sender_email == 'no-reply@notify.cloudflare.com' and '[Alert] Tunnel APT_Server' in subject:
+    elif from_email == 'no-reply@notify.cloudflare.com' and '[Alert] Tunnel APT_Server' in subject:
         pass # NOT SURE WHAT TO DO MAYBE JUST FLAG AS IMPORTANT
     
     else:
@@ -19,19 +19,10 @@ def preliminary_sort(message):
 
 
 def main():
-    preliminary_sort({
-        "subject": "Submission received",
-        "sender": {
-            "emailAddress": {
-                "name": "eLearning-Notification@utdallas.edu",
-                "address": "eLearning-Notification@utdallas.edu"
-            }
-        },
-        "body": {
-            "contentType": "html",
-            "content": "<html><head>\r\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"></head><body>Your submission was successful!<br><br>You can save or print this confirmation for your records. You can also view your receipts on your My Grades page.<br><br>Submission details:<br>Confirmation number: 9c0c1e34766f4b458b0e749de73fd472<br>Course: CS 3354.HON - Software Engineering - S26<br>Course ID: 2262-UTDAL-CS-3354-SECHON-24915<br>Title: Homework 1<br>Submission date: Feb 19, 2026 9:33 PM<br>Size of written submission: 0 bytes<br>Size and name of files received: CS 3354 HW1.pdf (236.6 kb) <br>Unique Item ID (for administrator use only): _8770459_1<br></body></html>"
-        },
-    })
+    messages = get_emails()
+    for email in messages:
+        preliminary_sort(email)
+        
     
 if __name__ == "__main__":
     main()
---
+++ src/outlook.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+def get_emails():
+    outlook_access_token = os.getenv("OUTLOOK_ACCESS_TOKEN")
+    messages = get_all_outlook_messages(
+        access_token=outlook_access_token,
+            select="id,subject,body,from",
+        )
+    return messages
+
+def get_all_outlook_messages(access_token: str, user_id: str = "me", **kwargs) -> list[dict]:
+    session = requests.Session()
+    session.headers.update({
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json",
+    })
+
+    params = {"$top": kwargs.get("top", 100)}
+    if "select" in kwargs:
+        params["$select"] = kwargs["select"]
+    if "filter" in kwargs:
+        params["$filter"] = kwargs["filter"]
+    if "orderby" in kwargs:
+        params["$orderby"] = kwargs["orderby"]
+
+    url = f"https://graph.microsoft.com/v1.0/{user_id}/mailFolders/inbox/messages"
+    all_messages = []
+
+    while url:
+        response = session.get(url, params=params if not all_messages else None)
+        response.raise_for_status()
+        data = response.json()
+        all_messages.extend(data.get("value", []))
+        url = data.get("@odata.nextLink")
+
+    return all_messages
